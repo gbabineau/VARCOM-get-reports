69f235a25d65790b8602b4a7e75a2ca21d31b50e
gbabineau <guy.babineau@gmail.com>
2025-03-28 16:30:10 -0400

    Correct integration tests

--- integration_test/create_report.sh
+++ integration_test/create_report.sh
@@ -1,4 +1,4 @@
 mkdir -p reports
-poetry run get_reports.get_reports --year 2021 --month 04
-poetry run get_reports.create_review_document  --input "reports/records_to_review_2021_04.json --output reports/review_2021_04.docx
+poetry run python -m get_reports.get_reports --year 2021 --month 04
+poetry run python -m  get_reports.create_review_document  --input reports/records_to_review_2021_04.json --output reports/review_2021_04.docx
 grep S85143932 reports/review_2021_04.docx
--- integration_test/generate_review_information.sh
+++ integration_test/generate_review_information.sh
@@ -1,3 +1,3 @@
 mkdir -p reports
-poetry run get_reports.get_reports --year 2021 --month 04
+poetry run python -m  get_reports.get_reports --year 2021 --month 04
 grep  S85143932 reports\records_to_review_2021_04.json
