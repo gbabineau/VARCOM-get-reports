8628e8b8e106b4e93dce0166b578e85c831aa933
gbabineau <guy.babineau@gmail.com>
2025-03-30 10:52:19 -0400

    Update file names

--- integration_test/create_report.sh
+++ integration_test/create_report.sh
@@ -1,5 +1,5 @@
 mkdir -p reports
 poetry run python -m get_reports.get_reports --year 2021 --month 04 --day 09 --region US-VA-003
-grep S85143932 reports/records_to_review_2021_04.json
+grep S85143932 reports/records_to_review_2021_04_09.json
 poetry run python -m  get_reports.create_review_document  --input reports/records_to_review_2021_04_09.json --output reports/review_2021_04_09.docx
-ls reports/review_2021_04.docx
+ls reports/review_2021_04_09.docx
